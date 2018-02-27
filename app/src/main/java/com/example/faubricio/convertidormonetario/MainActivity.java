56e7c1bf5bc4dd254af73f0750b1a61a16a564ff
Faubricio Forester <fkahuman@gmail.com>
2018-02-27 06:46:07 -0600

    Initial commit

---
+++ app/src/main/java/com/example/faubricio/convertidormonetario/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.faubricio.convertidormonetario;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
